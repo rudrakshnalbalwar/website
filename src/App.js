b6a10f381e48f08bb5026e7c976a5eb5b7ba36ba
Komal <laddha.kl@gmail.com>
2025-02-17 21:50:09 +0530

    update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import PrernaRegistration from './PrernaRegistration.js';
 
 function App() {
   return (
-    <Router basename="/website">
+    <Router basename="/">
       <Routes>
         <Route path="/" element={<NSSUnit />} />
         <Route path="/gallery" element={<Gallery />} />
